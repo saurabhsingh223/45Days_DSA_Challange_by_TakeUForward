47d2c92116ce558837dd330088e86edba28b05f3
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2026-06-03 01:34:39 +0530

    Time: 1 ms (95.15%), Space: 43.6 MB (15.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,21 +1,40 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+
         List<List<Integer>> triangle = new ArrayList<>();
-        for(int i = 0; i<numRows; i++){
-            List<Integer> list = new ArrayList<>();
-            for(int j = 0; j<=i; j++){
-                list.add(nCr(i,j));
-            }
-            triangle.add(list);
+        for(int i = 1; i<=numRows; i++){
+            triangle.add(addRow(i));
         }
         return triangle;
+
     }
-    public int nCr(int n, int r){
-        long result = 1;
-        for(int i = 0; i<r; i++){
-            result = result*(n-i);
-            result = result/(i+1);
+
+    public List<Integer> addRow(int n){
+        List<Integer> ans = new ArrayList<>();
+        int res = 1;
+        ans.add(res);
+        for(int i = 1; i<n; i++){
+            res = res*(n-i) / i;
+            ans.add(res);
         }
-        return (int) result;
+        return ans;
     }
+        //     // Brute Force Solution
+        //     List<List<Integer>> triangle = new ArrayList<>();
+        //     for(int i = 0; i<numRows; i++){
+        //         List<Integer> list = new ArrayList<>();
+        //         for(int j = 0; j<=i; j++){
+        //             list.add(nCr(i,j));
+        //         }
+        //         triangle.add(list);
+        //     }
+        //     return triangle;
+        // }
+        // public int nCr(int n, int r){
+        //     long result = 1;
+        //     for(int i = 0; i<r; i++){
+        //         result = result*(n-i);
+        //         result = result/(i+1);
+        //     }
+        //     return (int) result;
 }
