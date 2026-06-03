b2970ef139c359819303abf1327cfe0098130f66
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2026-06-04 01:34:49 +0530

    Time: 0 ms (100%), Space: 43.8 MB (32.22%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,16 +1,38 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int [][] ans = new int [matrix.length][matrix[0].length];
+        //Optimal Solution
         int n = matrix.length;
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[0].length; j++){
-                ans[j][n-1-i] = matrix[i][j];
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[0].length; j++){
-                matrix[i][j] = ans[i][j];
+
+        for(int i = 0; i<n; i++){
+            int left = 0, right = n-1;
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
             }
         }
     }
+         //Brute Force Solution
+        // int [][] ans = new int [matrix.length][matrix[0].length];
+        // int n = matrix.length;
+        // for(int i = 0; i<matrix.length; i++){
+        //     for(int j = 0; j<matrix[0].length; j++){
+        //         ans[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
+        // for(int i = 0; i<matrix.length; i++){
+        //     for(int j = 0; j<matrix[0].length; j++){
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+    // }
 }
