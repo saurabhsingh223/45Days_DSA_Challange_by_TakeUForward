37d26e6e1d3ffdf4433eba07f26665ccd350eb45
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2026-07-26 01:08:38 +0530

    Time: 1 ms (100%), Space: 43 MB (11.24%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -6,9 +6,20 @@ class Solution {
             list.add(digit);
             n /= 10;
         }
-        Collections.sort(list);
-        int max = list.get(list.size()-1);
-        int secMax = list.get(list.size()-2);
-        return max*secMax;
+        int lar = list.get(0);
+        int small = 0;
+        for(int i = 1; i< list.size(); i++){
+            if(list.get(i) >= lar){
+                small = lar;
+                lar = list.get(i);
+            }else if(list.get(i) < lar && list.get(i) > small){
+                small = list.get(i);
+            }
+        }
+        return small*lar;
+        // Collections.sort(list);
+        // int max = list.get(list.size()-1);
+        // int secMax = list.get(list.size()-2);
+        // return max*secMax;
     }
 }
