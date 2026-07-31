102a31613e3650bf7139757c82a2d93339a50936
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2026-08-01 00:47:41 +0530

    Time: 35 ms (43.98%), Space: 59.3 MB (45.9%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        // Optimal Sol
+        // Optimal Solution
+        
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int j = i + 1, k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.length-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0){
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1])
-                        k--;
+                    while(j < k && nums[j-1] == nums[j]) j++;
+                    while(j < k && nums[k+1] == nums[k]) k--;
+                } else if(sum < 0){
+                    j++;
+                }else{
+                    k--;
                 }
-                else if (sum < 0)
-                    j++;
-                else
-                    k--;
             }
         }
-        return ans;
+            return list;
+
         // Better Sol
 
         // Set<List<Integer>> ans = new HashSet<>();
@@ -45,7 +45,7 @@ class Solution {
 
         //Better Solution
 
-        // Brute
+        // Brute 
         // int n = nums.length;
         // for (int i = 0; i < n; i++) {
         //     for (int j = i; j < n; j++) {
